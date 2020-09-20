dad6d39bc95dea40e11c0da90d2dc2de29d43d89
changaloco <e.alamilla318@gmail.com>
2020-09-19 19:41:33 -0500

    Server start

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require ('morgan');
+const app = express();
+
+//settings 
+app.set('port',process.env.PORT || 3000);
+
+//middlewares 
+app.use(morgan('dev'));
+app.use(express.json());
+//routes
+
+//static files
+
+//starting server 
+
+app.listen(app.get('port'),() => {
+
+console.log('server on port  ${app.get('port')} ');
+
+});
