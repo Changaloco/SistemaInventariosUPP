1e26b18efe125ea98a282af659a1c945a47f4585
changaloco <e.alamilla318@gmail.com>
2020-09-28 16:56:55 -0500

    Instalaccion Nodemon

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ const express = require('express');
 const morgan = require ('morgan');
 const app = express();
 
-//settings 
-app.set('port',process.env.PORT || 3000);
 
-//middlewares 
-app.use(morgan('dev'));
-app.use(express.json());
+//settings
+app.set('port',process.env.PORT);
+//Middlewares
+
 //routes
 
-//static files
+//STatic Files
 
-//starting server 
-
-app.listen(app.get('port'),() => {
-
-console.log('server on port  ${app.get('port')} ');
-
-});
+//Starting the server 
