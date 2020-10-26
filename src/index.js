58d6b13eddf4751e09ddb4cb72358b6361fdecf9
changaloco <e.alamilla318@gmail.com>
2020-10-26 15:48:48 -0600

    Sqquelize add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const morgan = require ('morgan');
+const path = require('path');
 const app = express();
 
 
 //settings
 app.set('port',process.env.PORT || 3000);
 //Middlewares
-
+app.use(morgan('dev'));
+app.use(express.json());
 //routes
-
-//STatic Files
-
+app.use('/api/tasks',require('./routes/task.routes'));
+//Static Files
+app.use(express.static(path.join(__dirname,'public')));
 //Starting the server 
 app.listen(app.get('port'),() =>{
 console.log(`server on port ${app.get('port')}`);
